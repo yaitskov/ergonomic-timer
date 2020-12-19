a8709e3c1f5aa78af64faccc8af96633aedeef28
Daniil Iaitskov <daniil.iaitskov@soostone.com>
2020-12-19 13:09:07 +0300

    master heavy timer presalce 1024

--- mtimer.cpp
+++ mtimer.cpp
@@ -2,31 +2,36 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
-// #include <stdio.h>
+#include <avr/interrupt.h>
 
 typedef unsigned char b;
 typedef unsigned int ms;
-typedef int ps;
+typedef unsigned int mcs;
+typedef unsigned int minute;
 
 #define DIF(a,b) ((int) a - (int) b)
 
+#define LAST_MIN_IN_DAY (24U * 60U - 1U)
+#define LAST_MS_IN_MIN (60U * 1000U - 1U)
+
 class HumanTime {
 private:
-  ps _ps;
+  minute _minInDay; // 24 * 60
   ms _msInMin; // 1000 * 60
-  int _minInDay; // 24 * 60
+  mcs _microSecs;
 public:
-  // HumanTime() : _ps(0), _ms(0), _sec(0), _min(0), _hour(0) {}
-  void tick(ms msTicks, ps psTicks) {
-    _ps += psTicks;
-    if (_ps > 999) {
+  HumanTime(int minInDay, ms msInMin, mcs microSecs)
+    : _minInDay(minInDay), _msInMin(msInMin), _microSecs(microSecs) {}
+  void tick(ms msTicks, mcs psTicks) {
+    _microSecs += psTicks;
+    if (_microSecs > 999) {
       ++_msInMin;
-      _ps -= 1000;
+      _microSecs -= 1000;
     }
     _msInMin += msTicks;
-    while (_msInMin >= 60000U) {
+    while (_msInMin > LAST_MS_IN_MIN) {
       _msInMin = _msInMin - 60000U;
-      if (++_minInDay >= 24 * 60) {
+      if (++_minInDay > LAST_MIN_IN_DAY) {
         _minInDay = 0;
       };
     }
@@ -37,7 +42,7 @@ public:
     if (!d) {
       d = DIF(_msInMin, t2._msInMin);
       if (!d) {
-        d = DIF(_ps, t2._ps);
+        d = DIF(_microSecs, t2._microSecs);
       }
     }
     return d;
@@ -49,51 +54,77 @@ public:
 };
 
 
-HumanTime wallClock;
-HumanTime turnOnAfter;
-HumanTime turnOffAfter;
-// #define  turnOffAfter turnOnAfter
+HumanTime wallClock(0, 0, 0);
+HumanTime turnOnAfter(0, 0, 0);
+HumanTime turnOffAfter(LAST_MIN_IN_DAY, LAST_MS_IN_MIN, 0);
 
-#define ON_SET 1
-#define ON_AFTER_SET 2
-#define OFF_AFTER_SET 4
+#define PAUSE_SET 1
 b stateFlags = 0;
 
 // pins
 #define POWER_LED_PIN (1 << 0)
 #define POWER_PIN (1 << 1)
-#define BTN_PIN (1 << 2)
+#define ON_BTN_PIN (1 << 2)
+#define PAUSE_BTN_PIN (1 << 3)
+#define PAUSE_LED_PIN (1 << 4)
 
-#define TIMER_PRESCALE 64
+#define PRESCALE_BITS 0b101
+#define TIMER_PRESCALE 1024
 #define TIMER_OVERFLOW 256
 #define TIMER_PERIOD (TIMER_OVERFLOW * 1.0/(F_CPU / (1.0 * TIMER_PRESCALE)))
 #define TIMER_PERIOD_MS ((int) (TIMER_PERIOD * 1000))
 #define TIMER_PERIOD_PS ((int) (1000 * (TIMER_PERIOD * 1000 - TIMER_PERIOD_MS)))
 
-void timerInterrupt() {
+
+ISR(TIMER0_OVF_vect) {
   wallClock.tick(TIMER_PERIOD_MS, TIMER_PERIOD_PS);
 }
 
-void onOffBtnInterrupt() {
-  if (stateFlags & ON_SET) {
-    turnOffAfter = wallClock;
-  } else {
-    turnOnAfter = wallClock;
+ISR(INT0_vect) { // btn int
+  switch (PINB & (ON_BTN_PIN | PAUSE_BTN_PIN)) {
+  case ON_BTN_PIN: // toggle
+    if (wallClock > turnOnAfter && turnOffAfter > wallClock) {
+      // turn off because light is on
+      turnOffAfter = wallClock;
+    } else {
+      turnOnAfter = wallClock = HumanTime(0,0,0);
+      turnOffAfter = HumanTime(LAST_MIN_IN_DAY, LAST_MS_IN_MIN, 0);
+    }
+    break;
+  case PAUSE_BTN_PIN:
+    stateFlags ^= PAUSE_SET;
+    break;
   }
-  stateFlags ^= ON_SET;
 }
 
 int main(void) {
-  // timerInterrupt();
+  TCCR0B |= PRESCALE_BITS;
+  TCNT0 = 0; // reset counter 0
+  TIMSK |= 1; // enable timer overflows bit TOIE0
 
-  DDRB |= (1 << POWER_PIN) | (1 << POWER_LED_PIN);
-  PORTB |= ~((1 << POWER_PIN) | (1 << POWER_LED_PIN));
+  DDRB |= POWER_PIN | POWER_LED_PIN | PAUSE_LED_PIN;
+  PORTB |= ~(POWER_PIN | POWER_LED_PIN | PAUSE_LED_PIN);
+
+  sei();
 
   while (1) {
-    if ((stateFlags & ON_SET) && wallClock > turnOffAfter) {
-      PORTB &= ~(1 << POWER_PIN | 1 << POWER_LED_PIN);
-    } else if (!(stateFlags & ON_SET) && wallClock > turnOnAfter) {
-      PORTB |= 1 << POWER_PIN | 1 << POWER_LED_PIN;
+    if (stateFlags & PAUSE_SET) {
+      PORTB |= PAUSE_LED_PIN;
+      _delay_ms(300);
+      PORTB &= ~(POWER_PIN | POWER_LED_PIN | PAUSE_LED_PIN);
+      _delay_ms(700);
+    } else {
+      PORTB &= ~PAUSE_LED_PIN;
+      if (wallClock > turnOnAfter && turnOffAfter > wallClock) {
+        PORTB &= ~POWER_LED_PIN;
+        _delay_ms(300);
+        PORTB |= POWER_PIN | POWER_LED_PIN;
+      } else {
+        PORTB |= POWER_LED_PIN;
+        _delay_ms(300);
+        PORTB &= ~(POWER_PIN | POWER_LED_PIN);
+      }
+      _delay_ms(700);
     }
   }
 }
