ea5e3e5522165e2096af36b0c85fd2563a3148ed
Daniil Iaitskov <daniil.iaitskov@soostone.com>
2020-12-19 13:38:03 +0300

    master use PCINT for buttons

--- mtimer.cpp
+++ mtimer.cpp
@@ -62,11 +62,11 @@ HumanTime turnOffAfter(LAST_MIN_IN_DAY, LAST_MS_IN_MIN, 0);
 b stateFlags = 0;
 
 // pins
-#define POWER_LED_PIN (1 << 0)
-#define POWER_PIN (1 << 1)
-#define ON_BTN_PIN (1 << 2)
-#define PAUSE_BTN_PIN (1 << 3)
-#define PAUSE_LED_PIN (1 << 4)
+#define POWER_LED_PIN (1 << 0) //PB0
+#define POWER_PIN (1 << 1) // PB1
+#define TOGGLE_PIN (1 << 2)  // PB2
+#define PAUSE_BTN_PIN (1 << 3) // PB3
+#define PAUSE_LED_PIN (1 << 4) // PB4
 
 #define PRESCALE_BITS 0b101
 #define TIMER_PRESCALE 1024
@@ -80,20 +80,23 @@ ISR(TIMER0_OVF_vect) {
   wallClock.tick(TIMER_PERIOD_MS, TIMER_PERIOD_PS);
 }
 
-ISR(INT0_vect) { // btn int
-  switch (PINB & (ON_BTN_PIN | PAUSE_BTN_PIN)) {
-  case ON_BTN_PIN: // toggle
+volatile uint8_t historyPortB = 0xFF;
+
+ISR(PCINT0_vect) { // btn int
+  b changeBits = PINB ^ historyPortB;
+  historyPortB = PINB;
+
+  if (changeBits & TOGGLE_PIN) {
     if (wallClock > turnOnAfter && turnOffAfter > wallClock) {
       // turn off because light is on
       turnOffAfter = wallClock;
     } else {
+      // consequent on -> reset
       turnOnAfter = wallClock = HumanTime(0,0,0);
       turnOffAfter = HumanTime(LAST_MIN_IN_DAY, LAST_MS_IN_MIN, 0);
     }
-    break;
-  case PAUSE_BTN_PIN:
+  } else if (changeBits & PAUSE_BTN_PIN) {
     stateFlags ^= PAUSE_SET;
-    break;
   }
 }
 
@@ -101,6 +104,9 @@ int main(void) {
   TCCR0B |= PRESCALE_BITS;
   TCNT0 = 0; // reset counter 0
   TIMSK |= 1; // enable timer overflows bit TOIE0
+  GIMSK |= (1 << PCIE);     // set PCIE0 to enable PCMSK0 scan
+  // PCICR |= (1 << PCIE);     // set PCIE0 to enable PCMSK0 scan
+  PCMSK |= (1 << PCINT0);   // set PCINT0 to trigger an interrupt on state change
 
   DDRB |= POWER_PIN | POWER_LED_PIN | PAUSE_LED_PIN;
   PORTB |= ~(POWER_PIN | POWER_LED_PIN | PAUSE_LED_PIN);
